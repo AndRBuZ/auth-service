b60e8e0ff921bb2db43d765edb7756b9e31412f1
AndRBuZ <andrrrw56@gmail.com>
2025-06-15 16:17:55 +0500

    Refactor: Move hashing password functionality from user-service to auth-service

---
+++ src/main/java/com/foodorder/auth_service/dto/request/UserCreateDto.java
@@ -0,0 +1,3 @@
+package com.foodorder.auth_service.dto.request;
+
+public record UserCreateDto(String name, String email, String password) {}
--- src/main/java/com/foodorder/auth_service/service/AuthService.java
+++ src/main/java/com/foodorder/auth_service/service/AuthService.java
@@ -1,11 +1,15 @@
 package com.foodorder.auth_service.service;
 
 import com.foodorder.auth_service.dto.request.AuthRegisterDto;
+import com.foodorder.auth_service.dto.request.UserCreateDto;
 import com.foodorder.auth_service.dto.response.AuthResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class AuthService {
     private final RestTemplate restTemplate;
@@ -15,7 +19,19 @@ public class AuthService {
     }
 
     public ResponseEntity<AuthResponseDto> register(AuthRegisterDto dto) {
-        ResponseEntity<AuthResponseDto> resp = restTemplate.postForEntity("http://user-service:8081/users", dto, AuthResponseDto.class);
+        String password = hashPassword(dto.getPassword());
+
+        UserCreateDto userCreateDto = new UserCreateDto(
+                dto.getName(),
+                dto.getEmail(),
+                password
+        );
+
+        ResponseEntity<AuthResponseDto> resp = restTemplate.postForEntity("http://user-service:8081/users", userCreateDto, AuthResponseDto.class);
         return ResponseEntity.status(resp.getStatusCode()).body(resp.getBody());
+    }
+
+    private String hashPassword(String rawPassword) {
+        return BCrypt.hashpw(rawPassword, BCrypt.gensalt());
     }
 }
