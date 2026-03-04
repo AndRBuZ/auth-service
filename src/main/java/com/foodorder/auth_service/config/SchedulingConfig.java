9dd82c86a3e5270f3be14fb43f707592f79b0364
AndRBuZ <andrrrw56@gmail.com>
2026-03-04 19:32:31 +0500

    feat: add scheduler cleaner for invalid refresh tokens

--- src/main/java/com/foodorder/auth_service/config/SchedulingConfig.java
+++ src/main/java/com/foodorder/auth_service/config/SchedulingConfig.java
@@ -1,4 +1,9 @@
 package com.foodorder.auth_service.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
 public class SchedulingConfig {
 }
--- src/main/java/com/foodorder/auth_service/scheduler/CleanupScheduler.java
+++ src/main/java/com/foodorder/auth_service/scheduler/CleanupScheduler.java
@@ -1,4 +1,19 @@
 package com.foodorder.auth_service.scheduler;
 
+import com.foodorder.auth_service.scheduler.service.SchedulerRefreshTokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+@AllArgsConstructor
 public class CleanupScheduler {
+    private final SchedulerRefreshTokenService refreshTokenService;
+
+    @Scheduled(cron = "@weekly")
+    public void cleanupRefreshTokenTask() {
+        refreshTokenService.deleteInvalidToken(Instant.now());
+    }
 }
